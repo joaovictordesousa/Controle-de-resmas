5d31f315c9060f093bde54bfa2efeccbf7bd0566
joaovictodesousa <joaovictorsr26@gmail.com>
2023-02-13 13:47:31 -0300

    modified:   app/Http/Controllers/ImpressoesController.php 	modified:   app/Http/Controllers/historico_impressaoController.php 	modified:   app/Models/Solicitacao.php 	modified:   app/Models/User.php 	modified:   app/Models/impressoes.php 	modified:   database/migrations/2022_06_14_180808_create_table_solicitacoes.php 	modified:   database/migrations/2023_01_06_161945_create_impressoes_table.php

--- app/Http/Controllers/historico_impressaoController.php
+++ app/Http/Controllers/historico_impressaoController.php
@@ -16,11 +16,9 @@ class historico_impressaoController extends Controller
   public function show(Request $request)
     {
     //Definindo variaveis
-    $impressoes = Impressoes::orderby('id','DESC')->get();
-    // dd($impressoes);
-    $setores = setores::orderby('id')->get();
 
-    $impressoes = Impressoes::with('setores')->orderby('id','DESC')->get();
+
+    $setores = setores::orderby('id')->get();
     //Inputs
     $search = $request->input('id_setores');
     $users = Auth::user('name');
@@ -48,6 +46,54 @@ class historico_impressaoController extends Controller
         'quant_impressoes' => $quant_impressoes,
         'setores' => $setores ,
         'name' => $users,
+    ]);
+  }
+}
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Solicitacao;
+use App\Models\setores;
+use App\Models\Impressoes;
+use PhpParser\Node\Expr\Isset_;
+
+class historico_impressaoController extends Controller
+{
+
+  public function show(Request $request)  
+    {
+
+    //Definindo variaveis      
+
+      // return view('posts.index', compact('posts'));
+    
+    // dd($impressoes);
+    $setores = setores::orderby('id')->get();
+
+    //Inputs
+    $search = $request->input('id_setores');
+
+    //Somar
+    $quant_impressoes = Impressoes::sum('quant_impressoes');
+
+
+
+    if(!empty($search)){
+      $impressoes = impressoes::where('id_setores', '=', $search)->paginate(10)->withQueryString();
+        unset($quant_impressoes);
+        $quant_impressoes = 0;
+    }
+    else{
+      $impressoes = impressoes::with('setores')->paginate(10);
+    }
+    return view('historico-impressao',[
+        'impressoes' => $impressoes,
+        'quant_impressoes' => $quant_impressoes,
+        'setores' => $setores ,
+
     ]);
   }
 
