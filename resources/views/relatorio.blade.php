69de935ee92c15d3cd9303c1b849bee353193269
joaovictodesousa <joaovictorsr26@gmail.com>
2023-02-14 14:46:27 -0300

    modified:   resources/views/cadastro.blade.php 	modified:   resources/views/cadastro_impressao.blade.php 	modified:   resources/views/relatorio-impresao.blade.php 	modified:   resources/views/relatorio.blade.php

--- resources/views/relatorio.blade.php
+++ resources/views/relatorio.blade.php
@@ -11,7 +11,7 @@
     <div class="container">
         <div class="mh-100" style="width: 1000px; height: 1000px;">
             <div class="card border-dark" style="max-width: 700rem;">
-                <div class="card-header text-white" style="background-color: #044f84; text-align: center;" >RELATÓRIO RESMAS</div>
+                <div class="card-header text-white" style="background-color: #044f84; text-align: center;"><h4>RELATÓRIO RESMAS</h4></div>
                 <div class="card-body text-dark">
                     <a class="btn btn-outline-primary" href="/relatorio-impressao" role="button">RELATÓRIO DE IMPRESSÃO</a>
         <br><br>
