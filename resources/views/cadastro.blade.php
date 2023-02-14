a297ac9f747f0fd3f6f35390d8f3c327748cc7cc
joaovictodesousa <joaovictorsr26@gmail.com>
2023-02-14 14:40:12 -0300

    modified:   app/Http/Controllers/ImpressoesController.php 	modified:   resources/views/cadastro.blade.php 	modified:   resources/views/cadastro_impressao.blade.php 	modified:   resources/views/historico-impressao.blade.php 	modified:   resources/views/historico.blade.php 	modified:   resources/views/relatorio-impresao.blade.php 	modified:   resources/views/relatorio.blade.php

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -10,7 +10,7 @@
 <div class="container">
     <div class="mh-100" style="width: 1000px; height: 1000px;">
         <div class="card border-dark" style="max-width: 700rem;">
-            <div class="card-header text-white" style="background-color: #044f84;">Solicitação de resmas</div>
+            <div class="card-header text-white" style="background-color: #044f84; text-align: center;">SOLICITAÇÃO DE RESMAS</div>
             <div class="card-body text-dark">
                 <p class="card-text">
 
