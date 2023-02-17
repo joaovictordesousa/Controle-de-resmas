2965fbae8158e38f6d3b3e18721444256096f48d
joaovictodesousa <joaovictorsr26@gmail.com>
2023-02-17 13:23:49 -0300

    modified:   app/Http/Controllers/LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,30 +9,31 @@ use App\Models\User;
 
 class LoginController extends Controller
 {
-    /**
-     * Handle an authentication attempt.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function authenticate(Request $request)
-    {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
- 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
- 
-            return redirect()->intended('Cadastro');
-        }
- 
-        return back()->withErrors([
-            'email' => 'The provided credentials do not match our records.',
-        ])->onlyInput('email');
-    }
-    public function test(){
-        dd("oi");
-    }
+/**
+* Handle an authentication attempt.
+*
+* @param \Illuminate\Http\Request $request
+* @return \Illuminate\Http\Response
+*/
+public function authenticate(Request $request)
+{
+$credentials = $request->validate([
+'email' => ['required', 'email'],
+'password' => ['required'],
+]);
+
+if (Auth::attempt($credentials)) {
+$request->session()->regenerate();
+
+return redirect()->intended('Cadastro');
 }
+
+return back()->withErrors([
+'email' => 'The provided credentials do not match our records.',
+])->onlyInput('email');
+}
+public function test(){
+dd("oi");
+}
+}
+
